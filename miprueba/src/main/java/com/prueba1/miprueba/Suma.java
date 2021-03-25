7942534c08450be23e940046f9eb27fcc6a4429b
Felipe <Felipe@DESKTOP-D1NFVR7>
2021-03-25 16:31:09 -0300

    Respaldo en Git Actividad1

---
+++ miprueba/src/main/java/com/prueba1/miprueba/Suma.java
@@ -0,0 +1,14 @@
+package com.prueba1.miprueba;
+
+public class Suma {
+	
+	public double sumar(double a, double b) {
+		//Se va a multiplicar para que el test falle
+		return a*b;
+	}
+	
+	public double duplicar(double a) {
+		return a+a;
+	}
+
+}
